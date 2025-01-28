3d1ead21bc004a8c2a60e1f1cf700acf45f6e575
kitty14956590 <borbtrash@gmail.com>
2025-01-28 01:30:46 -0500

    modified: src/start.asm

--- src/start.asm
+++ src/start.asm
@@ -4,13 +4,6 @@ align 16
 extern __runtime_entry
 extern _start
 _start:
-	sub esp, 0xf0
-	mov ebp, esp
-	and ebp, 0xfffffff0
-	mov eax, 0
-	mov ebx, 0
-	mov ecx, 0
-	mov edx, 0
 	call __runtime_entry
 	mov ebx, eax
 	mov eax, 1
