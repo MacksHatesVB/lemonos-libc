19b50b08a27dc526390044d833632b9b63b10e12
kitty14956590 <borbtrash@gmail.com>
2025-01-29 01:33:47 -0500

    add file function definitions to stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -2,6 +2,10 @@
 
 #include <stdint.h>
 
+int open(char * name, int flags);
+ssize_t write(int fd, const void * buf, size_t count);
+ssize_t read(int fd, void * buf, size_t count);
+int close(int fd);
 void puts(char * text);
 void printf(char * fmt, ...);
 void outb(uint16_t port, uint8_t val);
