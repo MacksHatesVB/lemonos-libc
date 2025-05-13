4e2bc21588f8182667b31e01eecb801678775e0e
kitty14956590 <borbtrash@gmail.com>
2025-05-13 08:51:54 -0400

    fix open()

--- include/fcntl.h
+++ include/fcntl.h
@@ -19,7 +19,8 @@
 #define O_ASYNC         020000
 #define __O_LARGEFILE  0100000
 
-int open(char * name, int flags);
+//int open(char * name, int flags);
+int open(char * name, int flags, ...);
 ssize_t write(int fd, const void * buf, size_t count);
 ssize_t read(int fd, void * buf, size_t count);
 int close(int fd);
